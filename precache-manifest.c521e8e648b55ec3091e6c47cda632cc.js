abc524dbed81598d6431aea7a8b5b9f0804bf638
Shahidul Islam Majumder <dev@shahid.pro>
2019-12-26 10:27:03 +0600

    Updates

---
+++ precache-manifest.c521e8e648b55ec3091e6c47cda632cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478dac8a3a8c23184c3fb0d010305fcd",
+    "url": "/brf-client/index.html"
+  },
+  {
+    "revision": "9eb8b1a0abb8cc9fcaf8",
+    "url": "/brf-client/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "9eb8b1a0abb8cc9fcaf8",
+    "url": "/brf-client/static/js/2.d6e0e5f7.chunk.js"
+  },
+  {
+    "revision": "d1550ec930b0ddcf4886a70f53c49fee",
+    "url": "/brf-client/static/js/2.d6e0e5f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56174314eada52139101",
+    "url": "/brf-client/static/js/main.efd23b7e.chunk.js"
+  },
+  {
+    "revision": "8ed9369df2d699ad3035",
+    "url": "/brf-client/static/js/runtime-main.596ad9e2.js"
+  },
+  {
+    "revision": "e36e78ea57d884008acbc573b9ff55c3",
+    "url": "/brf-client/static/media/bg.e36e78ea.svg"
+  }
+]);
