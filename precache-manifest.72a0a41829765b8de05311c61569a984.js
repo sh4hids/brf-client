12a5305b5496d6caeb92f5709211d8fd5187dacf
Shahidul Islam Majumder <dev@shahid.pro>
2020-01-14 22:12:59 +0600

    Updates

---
+++ precache-manifest.72a0a41829765b8de05311c61569a984.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd36892b78889e7434fb821cee0e963",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aee1c2cb3523ada4f9cb",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "aee1c2cb3523ada4f9cb",
+    "url": "/static/js/2.5339ec1b.chunk.js"
+  },
+  {
+    "revision": "d1550ec930b0ddcf4886a70f53c49fee",
+    "url": "/static/js/2.5339ec1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d478526e588262ceedc7",
+    "url": "/static/js/main.4215296a.chunk.js"
+  },
+  {
+    "revision": "adbe057048013f342c9a",
+    "url": "/static/js/runtime-main.b911706c.js"
+  },
+  {
+    "revision": "e36e78ea57d884008acbc573b9ff55c3",
+    "url": "/static/media/bg.e36e78ea.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/brf-client/precache-manifest.4a058793cc78dfd3386ad8d6ac947d5c.js"
+  "/precache-manifest.72a0a41829765b8de05311c61569a984.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/brf-client/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
